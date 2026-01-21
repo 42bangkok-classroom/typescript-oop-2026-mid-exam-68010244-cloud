b9c4834f66888c98f4af58580f24161c7b99457d
Nene <68010244@kmitl.ac.th>
2026-01-21 16:23:56 +0700

    ss

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,24 @@
 // Write your code below
+// 2 point
+export{};
+
+
+export function numPat(num:number) : void {
+    if (isNaN(num) || num <= 0) {return;} {
+        for (let i = 1; i <= num; i++) {
+            let op = ""
+            for (let o = 0; o< num -i;o++) {
+            op += " "
+            }
+            for (let u = i; u >= 1;u--) {
+            op+= u
+            }
+        console.log(op)
+        }
+    }
+}
+
+
+const input = process.argv.slice(2);
+const num = Number(input)
+numPat(num);
