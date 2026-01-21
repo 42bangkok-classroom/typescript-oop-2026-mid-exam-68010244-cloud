f876d8562d762d269291487535e1cd5af8403c77
68010244@kmitl.ac.th <68010244@kmitl.ac.th>
2026-01-22 01:50:12 +0700

    ss

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -4,7 +4,7 @@ const input = process.argv.slice(2);
 const num = Number(input);
 
 for (var i = 1; i < num; i++) {
-    if (i % 5 == 0 || i % 3 == 0) {console.log("FizzBuzz"); continue;} 
+    if (i % 5 == 0 && i % 3 == 0) {console.log("FizzBuzz"); continue;} 
     else if (i % 3 == 0) {console.log("Fizz"); continue;}
     else if (i % 5 == 0) {console.log("Buzz"); continue;}
     else console.log(i);
